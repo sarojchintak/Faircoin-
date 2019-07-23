3f83c0437c27c680995e570bc0aedf05ab479c54
Saroj Chintakrindi <sarojchin@berkeley.edu>
2019-07-23 01:37:24 -0700

    Faircoin test

---
+++ x/nameservice/types/types.go
@@ -0,0 +1,35 @@
+package types
+
+import (
+	"fmt"
+	"strings"
+
+	sdk "github.com/cosmos/cosmos-sdk/types"
+)
+
+// Initial Starting Price for a name that was never previously owned
+var MinNamePrice = sdk.Coins{sdk.NewInt64Coin("nametoken", 1)}
+
+// Whois is a struct that contains all the metadata of a name
+// TODO: Refactor to reputation
+
+type Whois struct {
+	Value string         `json:"value"`
+	Owner sdk.AccAddress `json:"owner"`
+	Price sdk.Coins      `json:"price"`
+}
+
+// Returns a new Whois with the minprice as the price
+// TODO: Refactor to minRep 
+func NewWhois() Whois {
+	return Whois{
+		Price: MinNamePrice,
+	}
+}
+
+// implement fmt.Stringer
+func (w Whois) String() string {
+	return strings.TrimSpace(fmt.Sprintf(`Owner: %s
+Value: %s
+Price: %s`, w.Owner, w.Value, w.Price))
+}
